8225357ffafcc8805aac1aa67a8992e101b1c504
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:35:36 +0200

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	/* Parašyti programą, kuri leistų vartotojui įvesti savo masę ir ūgį, pagal gautus duomenis
+	    /* Parašyti programą, kuri leistų vartotojui įvesti savo masę ir ūgį, pagal gautus duomenis
         paskaičiuotų ir atvaizduotų jo kūno masės indeksą.
         KMI = masė (kg) / (ūgis(m))^2
-    */
+        */
+        float mase, ugis;
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Iveskite savo mase: ");
+        mase = in.nextFloat();
+        System.out.print("Iveskite savo ugi: ");
+        ugis = in.nextFloat();
+
+        System.out.print("KMI: " + mase / (ugis * ugis));
 
     }
 }
