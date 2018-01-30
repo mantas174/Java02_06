f0257fd388d39d454e0d8ecaf10c982cff5d363e
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:30:49 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* Parašyti programą, kuri leistų vartotojui įvesti savo masę ir ūgį, pagal gautus duomenis
+        paskaičiuotų ir atvaizduotų jo kūno masės indeksą.
+        KMI = masė (kg) / (ūgis(m))^2
+    */
+
+
+    }
+}
